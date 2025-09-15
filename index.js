1a1b2199f4c2cadf0fb93f19fd2c7626a5c0dcd3
Soe Tint Aung <soetintaunghz10@gmail.com>
2025-09-15 10:53:40 +0630

    fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Application Chnanges successfully roled out with GitHub Actions + ArgoCD | End-to-End GitOps CI/CD Pipeline on Kubernetes');
+  res.send('Soe Tint Aung + Jennie Kim');
 });
 
 app.listen(port, () => {
